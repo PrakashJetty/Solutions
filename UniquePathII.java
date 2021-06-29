2c6c1e57b2a779aa1dc495909a785d94994343a5
PrakashJetty <prakashjetty1@gmail.com>
2021-06-29 20:08:05 +0530

    Update UniquePathII.java

--- UniquePathII.java
+++ UniquePathII.java
@@ -1,11 +1,29 @@
-public class UniquePathII {
-	public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid.length == 1 && obstacleGrid[0].length == 1) {
+            return obstacleGrid[0][0] == 1? 0 : 1;
+        } 
+       int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
 		int[][] dp = new int[m][n];
-		for (int i = m -1 ; i >=0  ; --i) {
-			for (int j = n; j >= 0 ; --j) {
-				dp[i][j] = 
+
+		for (int i = m - 1; i >=0  ; --i) {
+			for (int j = n - 1; j >= 0 ; --j) {
+				if (i == m -1 && j == n - 1) {
+					dp[m - 1][n - 1] = 1;
+					continue;
+				}
+				if (obstacleGrid[i][j] == 0) {
+					if (i + 1 < m && obstacleGrid[i + 1][j] == 0)
+						dp[i][j] += dp[i + 1][j];
+
+					if (j + 1 < n && obstacleGrid[i][j + 1] == 0) {
+						dp[i][j] += dp[i][j + 1];
+					}
+				}
 			}
 		}
+        return dp[0][0];
         
     }
 }
